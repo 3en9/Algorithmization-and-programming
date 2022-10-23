e95d8fffc8568513d0e4a896c57703de44e79772
3en9 <85888021+3en9@users.noreply.github.com>
2022-10-24 02:01:50 +0300

    Update main.c

--- laba2/main.c
+++ laba2/main.c
@@ -7,15 +7,21 @@ int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
     printf("Лабораторная работа №2\n");
-    printf("Доп.задание\n");
-    int n, m;
+    printf("Задание №1\n");
+    int n, m, k;
     int t;
     do {
         printf("Введите размеры матрицы Z от 1 до %d\n",l_max);
         t = scanf("%d %d",&n,&m);
         while(getchar()!='\n');
     } while (n<=0 || n > l_max || m<=0 || m > l_max || t!=2);
+    do {
+        printf("Введите длину массива X от 1 до %d\n",l_max);
+        t = scanf("%d",&k);
+        while(getchar()!='\n');
+    } while (k<=0 || k > l_max || t!=1);
     int z[l_max][l_max];
+    int x[l_max];
     printf("Введите элементы матрицы Z\n");
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
@@ -24,32 +30,46 @@ int main() {
             } while (t!=1);
         }
     }
-
-    for(int j=1;j<=m;j++) {
-        char to_sort = 1;
-        int s = 1;
-        while(s<=n && to_sort!=0) {
-            if(z[s][j]==0) to_sort = 0;
-            s++;
-        }
-        if(to_sort==1) {
-            for (int i = 1; i < n; i++) {
-                for (int k = i + 1; k <= n; k++) {
-                    if (z[i][j] < z[k][j]) {
-                        int tmp = z[i][j];
-                        z[i][j] = z[k][j];
-                        z[k][j] = tmp;
-                    }
-                }
-            }
-        }
+    printf("Введите элементы массива X\n");
+    for(int i=1;i<=k;i++) {
+        do {
+            t = scanf("%d", &x[i]);
+        } while (t!=1);
     }
     printf("Вывод матрицы Z\n");
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) printf("%d ", z[i][j]);
         printf("\n");
     }
-
+    printf("Вывод массива X\n");
+    for(int i=1;i<=k;i++) printf("%d ",x[i]);
+    printf("\n\n");
+    int count = 0;
+    // находим максимум в каждой строке, проверяем его наличие в массиве
+    for(int i=1;i<=n;i++) {
+        int maximum = 1;
+        for(int j=2;j<=m;j++) {
+            if(abs(z[i][j]) > abs(z[i][maximum])) maximum = j;
+        }
+        char todel = 0;
+        int s = 1;
+        while(todel==0 && s<=k) {
+            if(x[s]==z[i][maximum]) todel = 1;
+            s++;
+        }
+        if(todel == 0) {
+            z[i][maximum] = 0;
+            count ++;
+        }
+    }
+    if(count!=0) {
+        printf("Вывод измененной матрицы\n");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) printf("%d ", z[i][j]);
+            printf("\n");
+        }
+    }
+    else printf("Обнуления не происходило, матрица не изменилась");
 
     return 0;
 }
